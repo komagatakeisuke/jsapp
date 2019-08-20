027ed400c352f8b0a10c84f23e85b38d7fedb62e
KomagataKeisuke <koma.study@gmail.com>
2019-08-21 02:47:58 +0900

    dbバックアップファイル追加

---
+++ pgbk001.sql
@@ -0,0 +1,86 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.5
+-- Dumped by pg_dump version 11.5
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: adminpack; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS adminpack WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION adminpack; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION adminpack IS 'administrative functions for PostgreSQL';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: products; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.products (
+    product_no integer,
+    name text,
+    price numeric
+);
+
+
+ALTER TABLE public.products OWNER TO postgres;
+
+--
+-- Name: weather; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.weather (
+    city character varying(80),
+    temp_lo integer,
+    temp_hi integer,
+    prcp real,
+    date date
+);
+
+
+ALTER TABLE public.weather OWNER TO postgres;
+
+--
+-- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.products (product_no, name, price) FROM stdin;
+1	Cheese	9.99
+2	rocket	777.77
+\.
+
+
+--
+-- Data for Name: weather; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.weather (city, temp_lo, temp_hi, prcp, date) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
